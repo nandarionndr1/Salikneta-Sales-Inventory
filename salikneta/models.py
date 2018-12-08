57f1cd95ecfc15c5b4d2bd542306bc8bea61986b
jayveegabriel <jaybriel19@gmail.com>
2018-12-08 14:50:16 +0800

    hello mark

--- salikneta/models.py
+++ salikneta/models.py
@@ -1,3 +1,111 @@
 from django.db import models
 
 # Create your models here.
+
+class Branch(models.Model):
+	idBranch = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=45)
+
+class Manager(models.Model):
+	idManager = models.AutoField(primary_key=True)
+	firstname = models.CharField(max_length=45)
+	lastname = models.CharField(max_length=45)
+	username = models.CharField(max_length=45)
+	password = models.CharField(max_length=45)
+	idBranch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+
+class Cashier(models.Model):
+	idCashier = models.AutoField(primary_key=True)
+	firstname = models.CharField(max_length=45)
+	lastname = models.CharField(max_length=45)
+	username = models.CharField(max_length=45)
+	password = models.CharField(max_length=45)
+	idBranch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+
+class Supplier(models.Model):
+	idSupplier = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=45)
+	contactNumber = models.CharField(max_length=45)
+	emailAddress = models.CharField(max_length=45)
+	website = models.CharField(max_length=45)
+	address1 = models.CharField(max_length=45)
+	address2 = models.CharField(max_length=45)
+	city = models.CharField(max_length=45)
+	province = models.CharField(max_length=45)
+	country = models.CharField(max_length=45)
+	postal = models.CharField(max_length=45)
+
+class Category(models.Model):
+	idCategory = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=45)
+	description = models.CharField(max_length=45)
+
+class Product(models.Model):
+	idProduct = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=45)
+	description = models.CharField(max_length=45)
+	suggestedUnitPrice = models.FloatField()
+	unitsInStock = models.IntegerField()
+	reorderLevel = models.IntegerField()
+	unitOfMeasure = models.CharField(max_length=45)
+	SKU = models.IntegerField()
+	barcode = models.CharField(max_length=45)
+	idCategory = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class PurchaseOrder(models.Model):
+	idPurchaseOrder = models.AutoField(primary_key=True)
+	idCashier = models.ForeignKey(Cashier, on_delete=models.CASCADE)
+	idSupplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+	orderDate = models.DateField()
+	expectedDate = models.DateField()
+
+class OrderLines(models.Model):
+	idOrderLines = models.AutoField(primary_key=True)
+	idPurchaseOrder = models.ForeignKey(PurchaseOrder, on_delete=models.CASCADE)
+	idProduct = models.ForeignKey(Product, on_delete=models.CASCADE)
+	qty = models.FloatField()
+
+class Delivery(models.Model):
+	idDelivery = models.AutoField(primary_key=True)
+	deliveryDate = models.DateField()
+	idPurchaseOrder = models.ForeignKey(PurchaseOrder, on_delete=models.CASCADE)
+
+class DeliveredProducts(models.Model):
+	idDeliveredProducts = models.AutoField(primary_key=True)
+	idOrderLines = models.ForeignKey(OrderLines, on_delete=models.CASCADE)
+	idDelivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)
+	qty = models.FloatField()
+
+class SalesInvoice(models.Model):
+	idSales = models.AutoField(primary_key=True)
+	idCashier = models.ForeignKey(Cashier, on_delete=models.CASCADE)
+	invoiceDate = models.DateField()
+	customer = models.CharField(max_length=45)
+
+class InvoiceLines(models.Model):
+	idSales = models.AutoField(primary_key=True)
+	idProduct = models.ForeignKey(Product, on_delete=models.CASCADE)
+	unitPrice = models.CharField(max_length=45)
+	qty = models.FloatField()
+
+class BackLoad(models.Model):
+	idBackload = models.AutoField(primary_key=True)
+	reason = models.CharField(max_length=45)
+	idCashier = models.ForeignKey(Cashier, on_delete=models.CASCADE)
+	backloadDate = models.DateField()
+
+class BackloadLines(models.Model):
+	idBackloadLines = models.AutoField(primary_key=True)
+	idProduct = models.ForeignKey(Product, on_delete=models.CASCADE)
+	qty = models.FloatField()
+
+class TransferOrder(models.Model):
+	idTransferOrder = models.AutoField(primary_key=True)
+	idCashier = models.ForeignKey(Cashier, on_delete=models.CASCADE)
+	transferDate = models.DateField()
+	expectedDate = models.DateField()
+
+class TransferLines(models.Model):
+	idTransferOrder = models.AutoField(primary_key=True)
+	idProduct = models.ForeignKey(Product, on_delete=models.CASCADE)
+	qty = models.FloatField()
